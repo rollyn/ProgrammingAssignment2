4013df62cc52a2f0e11b34faa7c4ee29ec1a6820
Ethanium <rollyn.moises@gmail.com>
2016-04-24 21:58:25 +0800

    cache matrix code

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,42 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## make the inverse of the matrix available in the cache
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        cache <- NULL
+        set <- function(y) {
+           x <<- y
+           cache <<- NULL
+        }
+        get <- function() {
+           x
+        }
+        setinv <- function(y) {
+           cache  <<- y
+        }
+        getinv <- function() {
+           cache
+        }
+        list(set = set,
+             get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
 ## Write a short comment describing this function
-
+## cacheSolve calcluates the inverse of the matrix
+## inverse stored in cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      
+        cache <- x$getinv()
+
+        if (!is.null(cache)) {
+             message("retrieved cached data")
+             return(cache)
+        }
+        data <- x$get()
+        cache <- solve(data)
+        x$setinv(inv)
+        cache
 }
